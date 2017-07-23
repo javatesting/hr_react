73dda3dad7e088a6e080e9cdb2305ed263e9d7fd
nik <kbodnar@ukr.net>
2017-07-23 22:44:47 +0300

    add

--- components/hr/person/PersonNewComponent.js
+++ components/hr/person/PersonNewComponent.js
@@ -2,17 +2,55 @@
  * Created by nik on 17.07.2017.
  */
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class PersonNewComponent extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            name: '',
+            last_name:'',
+            age: ''
+        }
 
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const formPayload = {
+            name: this.state.name,
+            last_name: this.state.last_name,
+            age: this.state.age
+        };
+
+        // console.log('Send this in a POST request:', formPayload);
+        // this.handleClearForm(e);
     }
 
     render() {
 
          return (
-             <div>PersonNewComponent</div>
+             <form onSubmit={this.handleSubmit}>
+                 <div className="table-responsive" style={{width: '30%'}}>
+                     <table className="table table-bordered">
+                         <tbody>
+                         <tr>
+                             <td><label>Name :</label></td>
+                             <td><input className="form-control" value={this.state.name}/></td>
+                         </tr>
+                         <tr>
+                             <td><label>Last name :</label></td>
+                             <td><input className="form-control" value={this.state.last_name}/></td>
+                         </tr>
+                         <tr>
+                             <td><label>Age :</label></td>
+                             <td><input className="form-control" value={this.state.age}/></td>
+                         </tr>
+                         </tbody>
+                     </table>
+                 </div>
+             </form>
          );
     }
 }
